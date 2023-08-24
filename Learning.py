8f01032d2c10c81946fa49ec73d9b374ff32d87a
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-08-24 03:51:47 +0300

    Test

--- Learning.py
+++ Learning.py
@@ -1,9 +1,3 @@
-a = int(input())
-b = int(input())
-c = 0
-d = 0
-for i in range (a, b + 1):
-    if i % 3 == 0:
-        c += 1
-        d += i
-print (d / c)
+a = input()
+a = a.upper()
+print((a.count("G") + a.count("C"))/len(a) * 100)
