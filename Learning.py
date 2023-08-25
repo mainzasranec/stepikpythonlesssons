d9cf60692003f8676e17baf01d090ae8ebfad1ba
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-08-25 16:31:49 +0300

    Test

--- Learning.py
+++ Learning.py
@@ -1,3 +1,15 @@
-a = input()
-a = a.upper()
-print((a.count("G") + a.count("C"))/len(a) * 100)
+s = "aaaabbcaa"
+counter = 0
+predbukva = s[0]
+for i in s:
+    if i == predbukva:
+        counter += 1
+    else:
+        print (predbukva + str(counter), end = "")
+    counter = 1
+    predbukva = i
+print (predbukva + str(counter))
+              
+    
+        
+    
