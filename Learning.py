857a35e205ac3fe6f369f44a3902f33b3af40996
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-08-30 02:02:01 +0300

    Test

--- Learning.py
+++ Learning.py
@@ -1,15 +1,5 @@
-s = "aaaabbcaa"
-counter = 0
-predbukva = s[0]
-for i in s:
-    if i == predbukva:
-        counter += 1
-    else:
-        print (predbukva + str(counter), end = "")
-    counter = 1
-    predbukva = i
-print (predbukva + str(counter))
-              
-    
-        
-    
+a = [int(i) for i in input().split()]
+total = 0
+for number in a:
+    total += number
+print(total)
