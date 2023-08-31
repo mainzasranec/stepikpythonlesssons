dd7b14e55476905d9ea901eb821a3f9b0c3201e0
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-09-01 02:52:56 +0300

    sdelal

--- Learning.py
+++ Learning.py
@@ -1,22 +1,11 @@
 ''' Ну и хуету я сделал'''
 numbers = [int(i) for i in input().split()]
-new_numbers = []
+newnumbers = []
 if len(numbers) == 1:
-    print (*numbers)
-for j in numbers:
-    if j == numbers[0]:
-        a = numbers[1] + numbers[-1]
-        new_numbers.append(a)
-    if j == numbers[1]:
-        a = numbers[0] + numbers[2]
-        new_numbers.append(a)
-    if j == numbers[2]:
-        a = numbers[1] + numbers[3]
-        new_numbers.append(a)
-    if j == numbers[3]:
-        a = numbers[2] + numbers[4]
-        new_numbers.append(a)
-    if j == numbers[4]:
-        a = numbers[3] + numbers[0]
-        new_numbers.append(a)
-print(*new_numbers)
+    print(*numbers)
+else:
+    for i in range(len(numbers) - 1):
+        newnumbers.append(numbers[i + 1] + numbers[i - 1])
+    else:
+        newnumbers.append(numbers[-2] + numbers[0])
+print(*newnumbers)
