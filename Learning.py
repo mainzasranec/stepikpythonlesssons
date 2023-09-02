31c7d27c7760808827ca5ef68c0929c5064e7e8b
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-09-02 21:14:14 +0300

    Test

--- Learning.py
+++ Learning.py
@@ -1,10 +1,4 @@
-numbers = [int(i) for i in input().split()]
-newnumbers = []
-if len(numbers) == 1:
-    print(*numbers)
-else:
-    for i in range(len(numbers) - 1):
-        newnumbers.append(numbers[i + 1] + numbers[i - 1])
-    else:
-        newnumbers.append(numbers[-2] + numbers[0])
-print(*newnumbers)
+numbers = sorted([int(i) for i in input().split()])
+for i in range(len(numbers) - 1):
+    if numbers[i] == numbers[i + 1]:
+        print(numbers[i + 1], end=" ")
