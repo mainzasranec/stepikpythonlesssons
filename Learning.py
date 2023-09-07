afa663b4a7499a733a3882f38f4bea842db51a84
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-09-07 18:29:58 +0300

    sdelal

--- Learning.py
+++ Learning.py
@@ -1,10 +1,5 @@
-numbers = sorted([int(i) for i in input().split()])
-newnumbers = []
-temp = []
-for i in range(len(numbers) - 1):
-    if numbers[i] == numbers[i + 1]:
-        newnumbers.append(numbers[i + 1])
-for x in newnumbers:
-    if x not in temp:
-        temp.append(x)
-print(*temp)
+a = int(input())
+s = []
+for i in range (a+1):
+    s += [i] * i 
+print(*s[:a])
