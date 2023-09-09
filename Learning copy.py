3737f88646e777ee524b15ae7ec141030c8ce64d
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-09-09 22:31:28 +0300

    deleted

--- Learning copy.py
+++
@@ -1,6 +0,0 @@
-matrix = [[9,5,3],[0,7,-1],[-5,2,9]]
-newmatrix = [[9,5,3],[0,7,-1],[-5,2,9]]
-for i in range (len(matrix)):
-    for j in range (len(matrix)):
-        newmatrix[i][j] = (matrix[i-1][j] + matrix[i+1-len(matrix)][j] + matrix[i][j-1] + matrix[i][j+1-len(matrix)])
-print(*newmatrix)
