7be00c4b80f7524a4927bc43b3c61b0f696e0341
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-09-16 02:48:03 +0300

    sdelal

--- Learning.py
+++ Learning.py
@@ -1,18 +1,21 @@
 def update_dictionary(d, key, value):
     if key in d:
-        d[key] = [value]
+        d[key] = d.get(key, []) + [value]
         return
     if key not in d:
-         d[2] = [value]
-         return
-    if d[2] not in d:
-        d[2] = [value]
+        d[key * 2] = d.get(key * 2, []) + [value]
+        return
+    if d[key * 2] not in d:
+        d[key * 2] = [value]
         return
 
-d = {}
-print (update_dictionary(d, 1, -1))
-print(d)
-update_dictionary(d, 2, -2)
-print(d)
-update_dictionary(d, 1, -3)
-print(d)
+x = {}
+    
+print(update_dictionary(x, 0, -5))  # None
+print(x)# {0: [-5]}  (0*0=2)
+update_dictionary(x, 1, -1)
+print(x)# {0: [-5], 2: [-1]} (тк индекса 1 нет создаем key*2=2)
+update_dictionary(x, 2, -2)
+print(x)# {0: [-5], 2: [-1, -2]} (тк индекс 2 есть добавляем -2 в него)
+update_dictionary(x, 3, -3)
+print(x)# 0: [-5], 2: [-1, -2], 6: [-3]}
