df10d1e09e9aaa7f4eea512d69c192df78106474
mainzasranec <Dan4ikZharov031@yandex.ru>
2023-09-17 17:57:19 +0300

    sdelal

--- Learning.py
+++ Learning.py
@@ -1,10 +1,9 @@
-def update_dictionary(d, key, value):
-    if key in d:
-        d[key] = d.get(key, []) + [value]
-        return
-    if key not in d:
-        d[key * 2] = d.get(key * 2, []) + [value]
-        return
-    if d[key * 2] not in d:
-        d[key * 2] = [value]
-        return
+dict = {}
+n = int(input())
+for i in range(n):
+    x = int(input())
+    if x in dict:
+        print(dict[x])
+    else:
+        dict[x] = f(x)
+        print(dict[x])
